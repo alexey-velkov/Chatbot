bb3663be006e28d058209b93841492fa53238e5e
Alexey Velkov <vielkov.oleksii@gmail.com>
2023-09-08 12:50:58 +0300

    add key

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const chatbotToggler = document.querySelector(".chatbot-toggler");
 const chatbotCloseBtn = document.querySelector(".close-btn");
 
 let userMessage;
-const API_KEY = "key-chatGPT";
+// const API_KEY = "key-chatGPT";
+const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
